212226baca2a72d5ca32979e99e82bdbb435f20f
bhargav.s <bhargavs.spaceo@gmail.com>
2022-11-15 16:15:04 +0530

    changes in view

--- OtpView/views.py
+++ OtpView/views.py
@@ -103,9 +103,10 @@ class OtpSendView(generics.GenericAPIView):
                                 os.getenv("TWILIO_AUTH_TOKEN",))
                 client.messages.create(to=mobile_number, from_=os.getenv(
                     "TWILIO_NUMBER",), body=message_to_broadcast)
-                
+
                 otp = Otp.objects.get(email=email)
-                SendMessages.objects.create(otp_id=otp, otp=random_otp, created_at = int(time.time()))
+                SendMessages.objects.create(
+                    otp_id=otp, otp=random_otp, created_at=int(time.time()))
                 return success_200(request, code=200, message="Otp send successfully.")
             else:
                 return error_400(request, code=400, message=list(serializer.errors.values())[0][0])
@@ -124,8 +125,14 @@ def home(request):
         response = json.dumps(response['responseData'])
     except KeyError:
         response = json.dumps(response['responseMessage'])
+    listotp = []
+    for row in Otp.objects.all():
+        listotp.append({
+            "id": str(row.id),
+            "username": row.username
+        })
 
-    context = {'data': response}
+    context = {"data": json.dumps(listotp)}
     return render(request, 'index.html', context=context)
 
 
@@ -164,6 +171,17 @@ def sendmessage(request, id):
 def send(request):
     if request.method == "POST":
         try:
+            url = os.getenv('SWAGGER_SERVER') + 'otpsend'
+            header = {
+                "Content-Type": 'application/json'
+            }
+            data = {
+                'email': 'bhargav@bhargav1.com',
+                'mobile_number': '7296909049',
+                'isd_code': '+91'
+            }
+            result = requests.post(url,  json=data, headers=header)
+            print(result.json())
             otp_id = request.POST.get('user_id')
             random_otp = request.POST.get('random_otp')
             otp = Otp.objects.get(id=otp_id)
@@ -172,7 +190,8 @@ def send(request):
                             os.getenv("TWILIO_AUTH_TOKEN",))
             client.messages.create(to=otp.mobile_number, from_=os.getenv(
                 "TWILIO_NUMBER",), body=message_to_broadcast)
-            SendMessages.objects.create(otp_id=otp, otp=random_otp, created_at = int(time.time()))
+            SendMessages.objects.create(
+                otp_id=otp, otp=random_otp, created_at=int(time.time()))
             messages.success(request, "Message sent successfuly.")
             return redirect('home')
         except TwilioRestException as e:
@@ -180,13 +199,16 @@ def send(request):
             return redirect("sendmessage", id=otp_id)
     return redirect('home')
 
+
 def showlist(request):
     sendmessagelist = SendMessages.objects.all()
     context = {'sendmessagelist': sendmessagelist}
     return render(request, 'send-message-list.html', context=context)
 
+
 def handler404(request, exception):
     return render(request, '404-found.html', status=404)
+
 
 def handler500(request):
     return render(request, '500-internal.html', status=500)
